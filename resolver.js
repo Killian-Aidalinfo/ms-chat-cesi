b02daed7da3793526a2e868c40301061aaf84a1f
Killian <killian.stein@aidalinfo.fr>
2023-10-24 23:10:43 +0200

    change prompt

--- resolver.js
+++ resolver.js
@@ -27,7 +27,7 @@ const resolvers = {
             console.log("🚀 Données non trouvées dans la base de données, interrogation de ms-gptlink...");
 
             // Faire une requête à ms-gptlink
-            const prompt = `Salut Guide ! Je suis en vacances à ${location}. J’aime les sorties de type : ${personality}. Que me proposes-tu ? Inclus moi un lien Google de l’établissement ou du lieux pour être sûr qu’il est bien ouvert. Tu peux étendre la zone à quelques kilomètres autour si il y a des trucs sympa à faire !`;
+            const prompt = `Salut Guide ! Je suis en vacances à ${location}. J’aime les sorties de type : ${personality}. Peux-tu me donner des recommandations numérotées sur ce que je pourrais faire ? N'oublie pas d'inclure un lien Google de l’établissement ou du lieu pour m'assurer qu'il est bien ouvert. Si nécessaire, tu peux étendre la zone à quelques kilomètres autour pour plus de suggestions. Merci !`;
 
             const gptResponse = await fetch('http://ms-gptlink:4000', {
                 method: 'POST',
